29aa6adfc0277f0915bc69d50b4ae3b6da937287
leen-gh <leen.ghaleb.d@gmail.com>
2026-03-25 16:36:45 +0300

    err fixing

--- client/src/index.js
+++ client/src/index.js
@@ -11,7 +11,7 @@ import App from './App';
 
 
 const httpLink = new HttpLink({
-  uri: 'http://localhost/servertest/public/graphql',
+  uri: 'https://scandiweb-store.great-site.net/sweb-backend/public/graphql',
   headers: {
     'Content-Type': 'application/json',
   },
